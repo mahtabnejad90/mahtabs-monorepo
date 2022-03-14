9d2ac656c4b7a845bb291c3c8f8c3c48d9465d69
Mahtab Nejad <mahtab@bahmannejad.com>
2022-03-14 11:20:38 +0000

    add some more practice directories

---
+++ Calculator/calculator.js
@@ -0,0 +1,7 @@
+let firstValue = 2;
+let secondValue = 4;
+let result = firstValue + secondValue;
+
+console.log("The value is: " + result);
+
+// runnning JS: node calculator.js
---
+++ Playground/error-debugging.js
@@ -0,0 +1,15 @@
+let outputObject = () => {
+    let testObject = {
+        owner : 'Bernard',
+        assistant : 'Manny',
+        neighbour : 'Fran'
+    };
+    
+    let objectItem = tsetObject.owner;
+    
+    console.log(objectItem);
+}
+
+outputObject();
+
+console.log('Script has completed');
